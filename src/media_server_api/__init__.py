f60a93906643232b3ccfca92db437401dad71590
ColorlessSaber <nuns-pastes-0s@icloud.com>
2026-09-15 14:00:46 -0700

    updated __init__.py for media_server_api folder
    Added in the RecentlyAddedRangeOptions and poster_downloader to __init__.py file.

--- src/media_server_api/__init__.py
+++ src/media_server_api/__init__.py
@@ -2,7 +2,13 @@
 __init__.py for media_server_api
 """
 __all__ = [
-    "recently_added_movies_and_tv_show"
+    "recently_added_movies_and_tv_show",
+    "RecentlyAddedRangeOptions",
+    "poster_downloader"
 ]
 
-from .recently_added_movies_and_tv_show import recently_added_movies_and_tv_show
+from .recently_added_movies_and_tv_show import (
+    recently_added_movies_and_tv_show,
+    RecentlyAddedRangeOptions
+)
+from .poster_downloader import poster_downloader
