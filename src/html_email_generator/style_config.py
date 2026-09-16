4cfc34da1d739bb2abdee38b3b00138de023fec3
ColorlessSaber <nuns-pastes-0s@icloud.com>
2026-09-16 10:26:59 -0700

    Removed dataclass to hold style configs
    When running ruff to check code it stated that it's not allowed to define a default value in a dataclass. Also, it was bad coding to use **{} when inputting data.
    The code has been updated to fix the issues stated above.

--- src/html_email_generator/style_config.py
+++ src/html_email_generator/style_config.py
@@ -1,8 +1,6 @@
 """
 Holds the style config settings for the HTML emails.
 """
-from typing import Optional
-from dataclasses import dataclass
 
 
 class RequiredStyleConfigAttrsMixin:
@@ -15,7 +13,7 @@ class RequiredStyleConfigAttrsMixin:
     """
     __slots__ = ('tag', 'selector_class')
 
-    def __init__(self, tag: Optional[str] = None, selector_class: Optional[str] = None):
+    def __init__(self, tag: str | None = None, selector_class: str | None = None):
         if tag is None:
             raise ValueError("No tag kwarg was provided")
         self.tag = tag
@@ -39,103 +37,123 @@ class StyleConfigBuilder(RequiredStyleConfigAttrsMixin):
 
         for key, value in vars(self).items():
             if key not in self.__slots__:
+                key = key.replace("_", "-")
+                if self.tag == ':root': # turn key into a variable
+                    key = "--" + key
+
                 attr_value = f"var({value})" if value.startswith("--") else value # handle CSS variables
+                attr_value = attr_value.replace("_", "-")
                 style_config += f"{key}: {attr_value}\n"
 
         style_config += "}"
 
         return style_config
 
+# Attributes that have double-underscores defines the start of the selector class.
+# Example: P__MEDIA_TITLES, p is the tag and media-titles is the selector class.
+ROOT = StyleConfigBuilder(
+    tag=':root',
+    email_background_color='#FFF8EC',
+    header_background_color='#FEC601',
+    poster_background_color='#DFE0DF',
+    text_section_background_color='#DFE0DF',
+)
+BODY = StyleConfigBuilder(
+    tag='body',
+    margin='0',
+    padding='0',
+    background_color='--email-background-color',
+)
 
-@dataclass
-class StyleConfigs:
-    """
-    Holds all the style config settings for each HTML tag.
+H_ONE: StyleConfigBuilder = StyleConfigBuilder(
+    tag='h1',
+    text_align='center',
+)
 
-    Attributes that have double-underscores defines the start of the selector class.
-    Example: P__MEDIA_TITLES, p is the tag and media-titles is the selector class.
-    """
-    ROOT: StyleConfigBuilder = StyleConfigBuilder(**{
-        'tag': ':root',
-        '--email-background-color': '#FFF8EC',
-        '--header-background-color': '#FEC601',
-        '--poster-background-color': '#DFE0DF',
-        '--text-section-background-color': '#DFE0DF',
-    })
-    BODY: StyleConfigBuilder = StyleConfigBuilder(**{
-        'tag': 'body',
-        'margin': '0',
-        'padding': '0',
-        'background-color': '--email-background-color',
-    })
-    H_ONE: StyleConfigBuilder = StyleConfigBuilder(**{
-        'tag': 'h1',
-        'text-align': 'center',
-    })
-    P__MEDIA_TITLES = StyleConfigBuilder(**{
-        'tag': 'p',
-        'selector_class': 'media-titles',
-        'text-align': 'center',
-        'font-size': '30px',
-        'font-weight': 'bold',
-        'margin-top': '-5%',
-    })
-    P__OVERFLOW = StyleConfigBuilder(**{
-        'tag': 'p',
-        'selector_class': 'overflow',
-        'text-align': 'center',
-        'font-size': '24px',
-        'line-height': '1.6',
-    })
+P__MEDIA_TITLES = StyleConfigBuilder(
+    tag='p',
+    selector_class='media-titles',
+    text_align='center',
+    font_size='30px',
+    font_weight='bold',
+    margin_top='-5%',
+)
 
-    # ~~ table tag and related style configs ~~
+P__CD_TITLE = StyleConfigBuilder(
+    tag='p',
+    selector_class='cd-title',
+    text_align='center',
+    font_size='30px',
+    font_weight='bold',
+    margin_top='-5%',
+)
 
-    TABLE__CONTAINER = StyleConfigBuilder(**{
-        'tag': 'table',
-        'selector_class': 'container',
-        'width': '100%',
-    })
-    TD__HEADER = StyleConfigBuilder(**{
-        'tag': 'td',
-        'selector_class': 'header',
-        'border': 'solid 1px black',
-        'border-radius': '15px',
-        'background-color': '--header-background-color',
-    })
-    TD__COLUMN = StyleConfigBuilder(**{
-        'tag': 'td',
-        'selector_class': 'column',
-        'border': 'solid 1px black',
-        'border-radius': '15px',
-        'background-color': '--poster-background-color',
-    })
-    TD__TEXT_SECTION = StyleConfigBuilder(**{
-        'tag': 'td',
-        'selector_class': 'column',
-        'border': 'solid 1px black',
-        'border-radius': '15px',
-        'background-color': '--text-section-background-color',
-    })
+P__OVERFLOW = StyleConfigBuilder(
+    tag='p',
+    selector_class='overflow',
+    text_align='center',
+    font_size='24px',
+    line_height='1.6',
+)
 
-    # ~~ img tag style configs ~~
+# ~~ table tag and related style configs ~~
 
-    IMG__PLEX_LOGO = StyleConfigBuilder(**{
-        'tag': 'table',
-        'selector_class': 'container',
-        'width': '100%',
-        'height': 'auto',
-        'border-radius': '30px',
-        'scale': '50%',
-        'margin-top': '-10%',
-        'margin-bottom': '-10%',
-    })
-    IMG__POSTER = StyleConfigBuilder(**{
-        'tag': 'img',
-        'selector_class': 'poster',
-        'width': '100%',
-        'height': 'auto',
-        'border-radius': '30px',
-        'scale': '50%',
-        'margin-top': '-30%',
-        'margin-bottom': '-30%',
-    })
+TABLE__CONTAINER = StyleConfigBuilder(
+    tag='table',
+    selector_class='container',
+    width='100%',
+)
+TD__HEADER = StyleConfigBuilder(
+    tag='td',
+    selector_class='header',
+    border='solid 1px black',
+    border_radius='15px',
+    background_color='--header-background-color',
+)
+TD__COLUMN = StyleConfigBuilder(
+    tag='td',
+    selector_class='column',
+    border='solid 1px black',
+    border_radius='15px',
+    background_color='--poster-background-color',
+)
+TD__TEXT_SECTION = StyleConfigBuilder(
+    tag='td',
+    selector_class='column',
+    border='solid 1px black',
+    border_radius='15px',
+    background_color='--text-section-background-color',
+)
+
+# ~~ img tag style configs ~~
+
+IMG__PLEX_LOGO = StyleConfigBuilder(
+    tag='table',
+    selector_class='container',
+    width='100%',
+    height='auto',
+    border_radius='30px',
+    scale='50%',
+    margin_top='-10%',
+    margin_bottom='-10%',
+)
+IMG__POSTER = StyleConfigBuilder(
+    tag='img',
+    selector_class='poster',
+    width='100%',
+    height='auto',
+    border_radius='30px',
+    scale='50%',
+    margin_top='-30%',
+    margin_bottom='-30%',
+)
+IMG__ALBUM_COVER = StyleConfigBuilder(
+    tag='img',
+    selector_class='album-cover',
+    width='100%',
+    height='auto',
+    border_radius='30px',
+    scale='45%',
+    margin_top='-20%',
+    margin_bottom='-20%',
+)
