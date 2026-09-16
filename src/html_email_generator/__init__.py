87848d3d87190fec6b32b308e9b454e73e754359
ColorlessSaber <nuns-pastes-0s@icloud.com>
2026-09-16 10:43:31 -0700

    added style configs to the __init__.py
    The html_email_generator folder's __init__ file has been updated to include the style config variables.

---
+++ src/html_email_generator/__init__.py
@@ -0,0 +1,35 @@
+"""
+__init__.py for html_email_generator
+"""
+
+__all__ = [
+    'BODY',
+    'H_ONE',
+    'IMG__ALBUM_COVER',
+    'IMG__PLEX_LOGO',
+    'IMG__POSTER',
+    'P__CD_TITLE',
+    'P__MEDIA_TITLES',
+    'P__OVERFLOW',
+    'ROOT',
+    'TABLE__CONTAINER',
+    'TD__COLUMN',
+    'TD__HEADER',
+    'TD__TEXT_SECTION',
+]
+
+from .style_config import (
+    BODY,
+    H_ONE,
+    IMG__ALBUM_COVER,
+    IMG__PLEX_LOGO,
+    IMG__POSTER,
+    P__CD_TITLE,
+    P__MEDIA_TITLES,
+    P__OVERFLOW,
+    ROOT,
+    TABLE__CONTAINER,
+    TD__COLUMN,
+    TD__HEADER,
+    TD__TEXT_SECTION,
+)
